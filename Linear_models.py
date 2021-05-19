b51d492cb3f79e687c2d2013726d74cb8b9f36dc
Filippo Bianchi <filippombianchi@gmail.com>
2021-05-19 16:53:47 +0200

    small changes

--- Linear_models.py
+++ Linear_models.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import RidgeClassifier, LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.preprocessing import StandardScaler
 
 
 # Load data
@@ -15,22 +16,29 @@ y = faults_clean.values[:,5].astype(np.int)
 feature_names = faults_clean.columns[1:5]
 
 # Subsample the non-fault class
-sampling_rate=6
+sampling_rate=60
 neg_idx = np.where(y==0)[0]
 delete_idx = np.setdiff1d(neg_idx, neg_idx[::sampling_rate])
 X = np.delete(X, delete_idx, axis=0)
 y = np.delete(y, delete_idx)
 
-# Standardize feats
-X_mean = np.mean(X, axis=0)
-X_std = np.std(X, axis=0)
-X = (X - X_mean)/X_std
 
 #Train/test split
 Xtr, Xte, ytr, yte = train_test_split(X, y, test_size=0.2, stratify=y)
 
-models = [RidgeClassifier(class_weight='balanced'), LogisticRegression(class_weight='balanced'), SVC(kernel='linear', class_weight='balanced')]
-model_names = ["RidgeClass", "LogisticReg", "LinSVC"]
+# Compute class proportions and weights
+neg, pos = np.bincount(ytr)
+yte_weights = np.copy(yte)
+yte_weights[yte_weights==0] = pos
+yte_weights[yte_weights==1] = neg
+
+# Normalize the features
+scaler = StandardScaler()
+Xtr = scaler.fit_transform(Xtr)
+Xte = scaler.transform(Xte)
+
+models = [RidgeClassifier(class_weight='balanced'), LogisticRegression(class_weight='balanced')] #, SVC(kernel='linear', class_weight='balanced')
+model_names = ["RidgeClass", "LogisticReg"] #, "LinSVC"
 
 res = []
 for model, name in zip(models, model_names):
@@ -44,10 +52,10 @@ for model, name in zip(models, model_names):
     # Compute metrics
     y_pred = model.fit(Xtr, ytr).predict(Xte)
     cm = confusion_matrix(yte, y_pred > 0.5)    
-    f1 = f1_score(yte, y_pred)
+    f1 = f1_score(yte, y_pred, sample_weight=yte_weights)
 
     print("{}:\n    True Negatives: {:d}\n    False Positives: {:d}\n    False Negatives: {:d}\n    True Positives: {:d}\n    F1 score: {:.3f}\n ".format(
-        name, cm[0][0], cm[0][1], cm[1][0], cm[1][1], f1  ))
+        name, cm[0][0], cm[0][1], cm[1][0], cm[1][1], f1 ))
 
 weights = np.vstack(res)
 weights = weights/weights.sum(axis=1)[:,None]
--- Nonlinear_models.py
+++ Nonlinear_models.py
@@ -6,9 +6,10 @@ from time import time
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 # Specify model and if we want to do feat selection
-model_name = "MLP"
+model_name = "OCSVM"
 cross_validation = False
 
 # Load data
@@ -25,13 +26,19 @@ delete_idx = np.setdiff1d(neg_idx, neg_idx[::sampling_rate])
 X = np.delete(X, delete_idx, axis=0)
 y = np.delete(y, delete_idx)
 
-# Standardize feats
-X_mean = np.mean(X, axis=0)
-X_std = np.std(X, axis=0)
-X = (X - X_mean)/X_std
-
 # Train/test split
 Xtr, Xte, ytr, yte = train_test_split(X, y, test_size=0.2, stratify=y)
+
+# Normalize the features
+scaler = StandardScaler()
+Xtr = scaler.fit_transform(Xtr)
+Xte = scaler.transform(Xte)
+
+# Compute class proportions and weights
+neg, pos = np.bincount(ytr)
+yte_weights = np.copy(yte)
+yte_weights[yte_weights==0] = pos
+yte_weights[yte_weights==1] = neg
 
 
 if model_name == "MLP":
@@ -46,8 +53,8 @@ elif model_name == "OCSVM":
 y_pred = model.fit(Xtr, ytr).predict(Xte)
 if model_name == "OCSVM":
     y_pred = (y_pred+1)/2
-cm = confusion_matrix(yte, y_pred > 0.5)    
-f1 = f1_score(yte, y_pred)
+cm = confusion_matrix(yte, y_pred)    
+f1 = f1_score(yte, y_pred, sample_weight=yte_weights)
 print("{}:\n    True Negatives: {:d}\n    False Positives: {:d}\n    False Negatives: {:d}\n    True Positives: {:d}\n    F1 score: {:.3f}\n ".format(
         model_name, cm[0][0], cm[0][1], cm[1][0], cm[1][1], f1  ))
 
@@ -77,10 +84,3 @@ if cross_validation:
     print("Features selected by backward sequential selection: "
           f"{feature_names[sfs_backward.get_support()]}")
     print(f"Done in {toc_bwd - tic_bwd:.3f}s")
-
-
-
-
-
-
-
